ed2bc87fb40b6e0bd2569aa27efdf6ab66e7e697
DeryaDursun <derdursun55@gmail.com>
2023-05-20 12:35:55 +0300

    main branch degisiklik

--- src/abc/C01.java
+++ src/abc/C01.java
@@ -3,5 +3,7 @@ package abc;
 public class C01 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("main branch degisklik 1.satir");
+        System.out.println("main branch degsklik 2.satir");
     }
 }
