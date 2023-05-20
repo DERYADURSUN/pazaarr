31f757bedecbd88bdb23f259ec8a419372330caf
DeryaDursun <derdursun55@gmail.com>
2023-05-20 12:32:46 +0300

    derya branch 2.degisklik

--- src/abc/C01.java
+++ src/abc/C01.java
@@ -2,6 +2,12 @@ package abc;
 
 public class C01 {
     public static void main(String[] args) {
+
         System.out.println("hello");
+
+        System.out.println("Thats it");
+
+        System.out.println("Derya brachinde ortak classda degisiklik yaptim");
+
     }
 }
